8f6bd908c3fced47b294f0efe9b413406cc68b7d
kaluluosi111 <kaluluosi@gmail.com>
2023-09-01 18:02:30 +0800

    调整项目

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,5 +5,5 @@ from functools import lru_cache
 @pytest.fixture
 def sample() -> str:
     print("sample called")
-    with open("tests/sample_en.md") as f:
+    with open("tests/sample/sample_en.md") as f:
         return f.read()
